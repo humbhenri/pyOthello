d688091d70d47e416036e69ab79629c4ef35f7ed
Humberto Pinheiro <humbhenri@gmail.com>
2024-08-22 10:56:41 -0300

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -17,17 +17,6 @@ in pkgs.mkShell rec {
     # Those are dependencies that we would like to use from nixpkgs, which will
     # add them to PYTHONPATH and thus make them accessible from within the venv.
     pythonPackages.pygame
-
-    # # In this particular example, in order to compile any binary extensions they may
-    # # require, the Python modules listed in the hypothetical requirements.txt need
-    # # the following packages to be installed locally:
-    # taglib
-    # openssl
-    # git
-    # libxml2
-    # libxslt
-    # libzip
-    # zlib
   ];
 
   # Run this command, only after creating the virtual environment
